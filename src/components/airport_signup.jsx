c8bfa00cc960a01cb03f118a766fd27758387031
vikas tadepu <41760133+vikastadepu@users.noreply.github.com>
2022-11-29 22:20:17 -0800

    Create airport_signup.jsx
    Airport signup

---
+++ src/components/airport_signup.jsx
@@ -0,0 +1,145 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {useNavigate} from 'react-router-dom';
+import { Fade } from '@mui/material';
+import Alert from '@mui/material/Alert';
+
+
+const theme = createTheme();
+
+export default function SignUpAirport() {
+  const [status,setStatus] = React.useState("");
+    const [message,setMessage] = React.useState("");
+    const [choice,setChoice] = React.useState(true);
+  const navigate = useNavigate();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    var a = {
+      "name": data.get('firstName') + data.get("lastName"),
+      email: data.get('email'),
+      password: data.get('password')
+    };
+  const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(a)
+  };
+  fetch('/api/airportsignup', requestOptions)
+      .then(response => response.json())
+      .then(data => 
+          {
+              if(data.status == "success"){
+                  navigate('/', {replace: true});
+                  window.location.reload(false);
+                  alert("Signed up successfully")
+                  // this.props.history.push("/");
+              }
+              setStatus("fail");
+              setMessage(data.message);
+              setChoice(false);
+              
+              
+          });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+                 {(status === "fail")?
+        <Fade in = {choice === false} timeout={3000}>
+        <Alert severity="error">{message}</Alert>
+                </Fade>:null}
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Airport Sign up
+          </Typography>
+          <Box component="form"  onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="/login/airport" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
