ddb5cc4e10fb2fbfc3e39f950ef4c605d59c06b7
thanujkumarj <99846736+thanujkumarj@users.noreply.github.com>
2022-11-29 22:21:09 -0800

    Update allbaggages.jsx

--- src/components/allBaggages.jsx
+++ src/components/allBaggages.jsx
@@ -1,1 +1,20 @@
-
+import React from "react";
+import BasicCard from "./card";
+import BasicFlightCard from "./flightcard";
+import BasicBaggageCard from "./baggagecard";
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
+export default function BaggagesList(props){
+    // window.location.reload(false);
+    return (
+        <div>
+            <Button component = {Link} to = "/addbaggage">Add Baggage</Button>
+            <br></br>
+             {(props.data!=null && props.data.length !=0)?
+             <BasicBaggageCard data = {props.data.data}></BasicBaggageCard>
+          
+        :null}
+        </div>
+       
+    );
+};
