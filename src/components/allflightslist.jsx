153b81dab4d802d91fe0803e33e79f371496f641
thanujkumarj <99846736+thanujkumarj@users.noreply.github.com>
2022-11-29 22:20:14 -0800

    Update allflights.jsx

--- src/components/allflightslist.jsx
+++ src/components/allflightslist.jsx
@@ -1,1 +1,16 @@
-
+import React from "react";
+import BasicCard from "./card";
+import BasicFlightCard from "./flightcard";
+export default function FlightsList(props){
+    // window.location.reload(false);
+    return (
+        <div>
+             {(props.data!=null && props.data.length !=0)?
+             
+           <BasicFlightCard data = {props.data} time = {props.time}></BasicFlightCard> 
+           
+        :null}
+        </div>
+       
+    );
+};
